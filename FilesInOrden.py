d30c2679566b0e85374698d828e77e85152a2c6a
HaR3s <zeidel07.gatcia@gmail.com>
2025-05-13 01:55:06 -0400

    kfk

--- FilesInOrden.py
+++ FilesInOrden.py
@@ -738,9 +738,9 @@ class FileOrganizerGUI(tk.Tk):
         selected = self.format_tree.selection()
         if selected:
             self.format_tree.delete(selected[0])
-            self.log(selected[0])
-            self.profiles["default"]["formatos"].pop(selected[0])
-            self.log(self.profiles["default"]["formatos"])
+            self.log(self.format_tree.get_children(selected[0]))
+            # self.profiles["default"]["formatos"].pop(selected[0])
+            # self.log(self.profiles["default"]["formatos"])
 
     def apply_appearance_settings(self):
         """Aplica todos los cambios de apariencia"""
